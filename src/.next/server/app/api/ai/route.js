77c5aa9e3eee47f074f8b93cc0eaa430f5eb782d
Tue Linh <mrperfect@Cerise.local>
2026-05-05 06:07:28 -0400

    Remove tracked Next build cache

--- src/.next/server/app/api/ai/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/ai/route.js")
-R.c("server/chunks/[root-of-the-server]__0teih4t._.js")
-R.c("server/chunks/node_modules_0_z-k9t._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/src__next-internal_server_app_api_ai_route_actions_1304~l1.js")
-R.m(59579)
-module.exports=R.m(59579).exports
